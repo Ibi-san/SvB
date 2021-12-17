7af68727378e25258832f94ea8374ca1eb83f5c9
Ibi-san <89692342+Ibi-san@users.noreply.github.com>
2021-12-18 02:34:43 +0300

    Added Food
    FoodPrefab with Text

---
+++ Assets/Script/Food.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public int Value;
+    public TextMesh Text;
+
+    private void Awake()
+    {
+        Text.text = Value.ToString();
+    }
+}
