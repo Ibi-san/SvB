5e068787cf86c2c482742be6d8aeb6cfcb66f76c
Ibi-san <89692342+Ibi-san@users.noreply.github.com>
2021-12-18 03:33:31 +0300

    Added PlayerMoving
    Smooth tail moving

---
+++ Assets/SnakeMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    public List<Transform> bodyParts = new List<Transform>();
+    public float minDistance = 0.25f;
+    public int beginSize;
+    public float speed = 1;
+
+    public GameObject bodyprefabs;
+
+    private float dis;
+    private Transform curBodyPart;
+    private Transform PrevBodyPart;
+
+    void Start()
+    {
+        for (int i = 0; i < beginSize - 1; i++)
+        {
+
+            AddBodyPart();
+
+        }
+    }
+
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKey(KeyCode.Q))
+            AddBodyPart();
+    }
+
+    public void Move()
+    {
+        float curspeed = speed;
+        bodyParts[0].Translate(bodyParts[0].forward * Time.smoothDeltaTime * curspeed, Space.World);
+
+        for (int i = 1; i < bodyParts.Count; i++)
+        {
+
+            curBodyPart = bodyParts[i];
+            PrevBodyPart = bodyParts[i - 1];
+
+            dis = Vector3.Distance(PrevBodyPart.position, curBodyPart.position);
+
+            Vector3 newpos = PrevBodyPart.position;
+
+            newpos.y = bodyParts[0].position.y;
+
+            float T = Time.deltaTime * dis / minDistance * curspeed;
+
+            if (T > 0.5f)
+                T = 0.5f;
+            curBodyPart.position = Vector3.Slerp(curBodyPart.position, newpos, T);
+        }
+    }
+
+
+    public void AddBodyPart()
+    {
+        Transform newpart = (Instantiate(bodyprefabs, bodyParts[bodyParts.Count - 1].position, bodyParts[bodyParts.Count - 1].rotation) as GameObject).transform;
+        newpart.SetParent(transform);
+        bodyParts.Add(newpart);
+    }
+}
