6420df3be132dbbe38b47b56555bbe3d1d2a8da9
Ibi-san <89692342+Ibi-san@users.noreply.github.com>
2021-12-20 19:42:36 +0300

    Added random value to blocks

--- Assets/Script/Block.cs
+++ Assets/Script/Block.cs
@@ -5,6 +5,11 @@ public class Block : MonoBehaviour
     public int Value;
     public TextMesh Text;
 
+    private void Start()
+    {
+        Value = Random.Range(1, 30);
+}
+
     private void Update()
     {
         Text.text = Value.ToString();
